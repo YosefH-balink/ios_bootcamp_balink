a98cf90b6cc080596d66e1cc404513e00c438c2d
YosefH-balink <136228996+YosefH-balink@users.noreply.github.com>
2023-06-12 13:34:51 +0300

    add exercises 2
    add exercises 2 to process inputs to determine the larger slice.

--- Exercises/week_1/day_2.playground/Contents.swift
+++ Exercises/week_1/day_2.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-// compute slice sizes which returns nil for invalid input
+//1. compute slice sizes which returns nil for invalid input
 func sliceSize(diameter: Double?, slices: Int?) -> Double? {
     
     if IsNilOrDiameterNegativeOrSlicesLessThen1 (diameter: diameter, slices: slices){
@@ -33,3 +33,39 @@ sliceSize(diameter: 16, slices: 12)
 sliceSize(diameter: nil, slices: 8)
 // => nil
 
+
+//2. Process inputs to determine the larger slice.
+func biggestSlice(diameterA: String, slicesA: String, diameterB: String, slicesB: String) -> String {
+    
+    let diameter1 = Double(diameterA)
+    let slices1 = Int(slicesA)
+    let diameter2 = Double(diameterB)
+    let slices2 = Int(slicesB)
+    
+    let sliceSize1 = sliceSize(diameter: diameter1, slices: slices1)
+    let sliceSize2 = sliceSize(diameter: diameter2, slices: slices2)
+    
+    let message: String
+    
+    if (sliceSize1 == nil && sliceSize2 == nil){
+        message = "Neither slice is bigger"
+    } else if (sliceSize1 != nil && sliceSize2 == nil) {
+        message = "Slice A is bigger"
+    }else if (sliceSize1 == nil && sliceSize2 != nil) {
+        message = "Slice B is bigger"
+    }else if (sliceSize1! > sliceSize2!) {
+        message = "Slice A is bigger"
+    }else  {
+        message = "Slice B is bigger"
+    }
+    
+       return message
+}
+
+
+biggestSlice(diameterA: "10", slicesA: "6", diameterB: "14", slicesB: "12")
+// => Slice A is bigger
+biggestSlice(diameterA: "10", slicesA: "6", diameterB: "12", slicesB: "8")
+// => Slice B is bigger
+biggestSlice(diameterA: "Pepperoni", slicesA: "6", diameterB: "Sausage", slicesB: "12")
+// => Neither slice is bigger

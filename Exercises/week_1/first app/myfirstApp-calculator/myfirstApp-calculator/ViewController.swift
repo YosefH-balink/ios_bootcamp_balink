58d51b68a7457209cc78bafc8c9c6da7c5875515
YosefH-balink <136228996+YosefH-balink@users.noreply.github.com>
2023-06-14 15:33:00 +0300

    updated the app

--- Exercises/week_1/first app/myfirstApp-calculator/myfirstApp-calculator/ViewController.swift
+++ Exercises/week_1/first app/myfirstApp-calculator/myfirstApp-calculator/ViewController.swift
@@ -9,42 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
- 
     @IBOutlet weak var line1: UITextField!
-    
     @IBOutlet weak var line2: UITextField!
     
     
     var calculaterLine1: Double = 0 {
         didSet {
-            line1.text = String(calculaterLine1)
+            line1.text = String(format:"%0.0f", calculaterLine1)
         }
     }
     
     var calculaterLine2: Double = 0  {
         didSet {
-            line2.text = String(calculaterLine2)
+            line2.text = String(format:"%0.0f", calculaterLine2)
         }
     }
     
-    var preestAporater:Bool = false
-    
-   
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        line1.text = String(0)
-        // Do any additional setup after loading the view.
     }
-    
-    
     
     @IBAction func numberPressed(_ sender: UIButton) {
         let num = Double(sender.currentTitle!)
-        if aporatersType != "" && eqwalsPressed == true{
-            calculaterLine1 = 0
-            calculaterLine2 = 0
-        }
+
         if !preestAporater{
             if calculaterLine1 == 0 {
                 calculaterLine1 = num!
@@ -63,29 +50,25 @@ class ViewController: UIViewController {
         }
     }
     
-    
-    
     var aporatersType: String = ""
-    
+    var preestAporater:Bool = false
     @IBAction func aporatersPressed(_ sender: UIButton) {
         aporatersType = (sender.currentTitle!)
         preestAporater = !preestAporater
-        print(aporatersType)
-        
-        
     }
+    
     var eqwalsPressed:Bool = false
     @IBAction func eqwals(_ sender: Any) {
         eqwalsPressed = true
         switch aporatersType {
         case "+":
-            line2.text = String(calculaterLine1 + calculaterLine2)
+            calculaterLine2 = calculaterLine1 + calculaterLine2
             calculaterLine1 = 0
         case "-":
-            line2.text = String(calculaterLine1 - calculaterLine2)
+            calculaterLine2 = calculaterLine1 - calculaterLine2
             calculaterLine1 = 0
         case "x":
-            line2.text = String(calculaterLine1 * calculaterLine2)
+            calculaterLine2 = calculaterLine1 * calculaterLine2
             calculaterLine1 = 0
         case "/":
             line2.text = String(calculaterLine1 / calculaterLine2)
@@ -93,9 +76,15 @@ class ViewController: UIViewController {
         default: break
             
         }
+        line1.text = ""
         print(calculaterLine1,aporatersType,calculaterLine2)
-        
     }
     
+    @IBAction func clear(_ sender: Any) {
+        preestAporater = false
+        calculaterLine1 = 0
+        calculaterLine2 = 0
+        line2.text = ""
+    }
 }
 

31481d33b35e7f69db56ca959816e9fe474035f7
YosefH-balink <136228996+YosefH-balink@users.noreply.github.com>
2023-06-15 19:11:02 +0300

    ended multi-part Challenge (for today...)

---
+++ Exercises/Multi-Part Challenge Week-1/Multi-Part Challenge Week-1/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Multi-Part Challenge Week-1
+//
+//  Created by Balink on 15/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginButton.isEnabled = false
+        self.title = "Login"
+    }
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    var userName:String=""
+    var password:String = ""
+    var isValidName:Bool = false
+    var isValidPassword:Bool = false
+    @IBAction func nameInput(_ sender: UITextField) {
+                if let name = sender.text {
+                    userName = name
+                }
+        isValidName = userName.isValidWith(regex: "^[A-Za-z]+$")
+        if isValidName && userName.count > 4 && isValidPassword{
+            loginButton.isEnabled = true
+        }else{
+            loginButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func passwordInput(_ sender: UITextField) {
+        if let inputPassword = sender.text {
+               password = inputPassword
+           }
+        isValidPassword = password.isValidWith(regex: "^[A-Za-z0-9]+$")
+        if isValidPassword && password.count > 6 && isValidName{
+            loginButton.isEnabled = true
+        }else{
+            loginButton.isEnabled = false
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let Welcome_Page = segue.destination as? Welcome_PageViewController
+        else { return }
+        Welcome_Page.name = self.userName
+    }
+}
+
+extension String {
+func isValidWith(regex: String) -> Bool {
+    guard let gRegex = try? NSRegularExpression(pattern: regex) else {
+        return false
+    }
+    let range = NSRange(location: 0, length: self.utf16.count)
+    
+    if gRegex.firstMatch(in: self, options: [], range: range) != nil {
+        return true
+    }
+    return false
+}
+    
+}
+
+
+
+
+
+
---
+++ Exercises/Multi-Part Challenge Week-1/Multi-Part Challenge Week-1/Welcome-PageViewController.swift
@@ -0,0 +1,40 @@
+//
+//  Welcome-PageViewController.swift
+//  Multi-Part Challenge Week-1
+//
+//  Created by Balink on 15/06/2023.
+//
+let members = ["Yonathan Goriachnick=yonig@balink.net", "Alexander Bord=alexanderb@balink.net", "Amiram Elzam=amirame@balink.net", "Avraham Lewensohn=avrahaml@balink.net", "Benjamin Halimi=benjaminh@balink.net", "Eliachar Feig=eliacharf@balink.net", "Emmanuel Berdugo=emmanuelb@balink.net", "Ethan Benzennou=ethanb@balink.net", "Laura Atlan=lauraat@balink.net", "Mendi Brami=mendib@balink.net", "Moishie Krohn=moshek@balink.net", "Yakov Touati=yakovt@balink.net", "Yankel Melloul=yankelm@balink.net", "Yitzchak Schechter=yitzchaks@balink.net", "Yona Harel=yonah@balink.net", "Yosef Heifetz=yosefh@balink.net"]
+
+import UIKit
+
+class Welcome_PageViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return members.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "normalCall", for: indexPath)
+        cell.textLabel?.text =  members[indexPath.row]
+        cell.imageView?.image = UIImage(named: "BA-Link")
+        cell.accessoryType = .none //.disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+       return "BA-Link Members"
+    }
+    
+    public var name: String? = nil
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Welcome \(self.name ?? "" )"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var nameLabel: UILabel!
+   
+
+}

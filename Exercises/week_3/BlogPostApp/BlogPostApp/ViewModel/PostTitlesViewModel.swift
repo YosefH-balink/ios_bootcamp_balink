755e8170bc28a0da5bf53be487581b9e17b34ea8
YosefH-balink <136228996+YosefH-balink@users.noreply.github.com>
2023-06-28 18:34:34 +0300

    add Combine!!!

--- Exercises/week_3/BlogPostApp/BlogPostApp/ViewModel/PostTitlesViewModel.swift
+++ Exercises/week_3/BlogPostApp/BlogPostApp/ViewModel/PostTitlesViewModel.swift
@@ -6,28 +6,58 @@
 //
 
 import Foundation
+import Combine
 
-class PostTitlesViewModel {
+class PostTitlesViewModelWithCombine: ObservableObject {
     
-    private var posts:[Post]?
-   
-    func getPostTitles() async throws{
-        do {
-            try posts = await DataService.shared.fetchPosts()
-        }
-        catch let error as ServerError {
-            print(error)
-        }
+    @Published private var posts:[Post] = []
+    var observer: AnyCancellable?
+    
+    func getPostFromServer(){
+        observer = DataServiceWithCombine.fetcPost()
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                
+            }, receiveValue: {[weak self] value in
+                print(value)
+                self?.posts = value
+            })
     }
-
+    
     func getnumberOfRows() -> Int{
-        return self.posts?.count ?? 0
+        return self.posts.count
     }
     
     func getCellTitle(index: Int) -> CellTitleViewModel{
-        let cellTitleViewModel = CellTitleViewModel(post: posts![index])
+        let cellTitleViewModel = CellTitleViewModel(post: posts[index])
         return cellTitleViewModel
     }
 }
+
+
+
+// old PostTitlesViewModel with no Combine
+//
+//class PostTitlesViewModel {
+//    private var posts:[Post]?
+//   
+//    func getPostTitles() async throws{
+//        do {
+//            try posts = await DataServiceWithAsyncAwait.shared.fetchPosts()
+//        }
+//        catch let error as ServerError {
+//            print(error)
+//        }
+//    }
+//
+//    func getnumberOfRows() -> Int{
+//        return self.posts?.count ?? 0
+//    }
+//    
+//    func getCellTitle(index: Int) -> CellTitleViewModel{
+//        let cellTitleViewModel = CellTitleViewModel(post: posts![index])
+//        return cellTitleViewModel
+//    }
+//}
 
 
--- Exercises/week_3/BlogPostApp/BlogPostApp/viewControllers/PostTitlesViewController.swift
+++ Exercises/week_3/BlogPostApp/BlogPostApp/viewControllers/PostTitlesViewController.swift
@@ -6,33 +6,67 @@
 //
 
 import UIKit
+import Combine
 
-class PostTitlesViewController:UIViewController,  UITableViewDelegate, UITableViewDataSource {
+
+
+class PostTitlesViewController:UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var titleTable: UITableView!
     
-    var myTitles: PostTitlesViewModel?
-    
+    var viewModel = PostTitlesViewModelWithCombine()
+    private var cancellable: AnyCancellable?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        Task{
-            do {
-                myTitles = PostTitlesViewModel()
-                try await myTitles?.getPostTitles()
-                titleTable.reloadData()
-            } catch let error as ServerError {
-                print(error)
+        viewModel.getPostFromServer()
+        cancellable = viewModel.objectWillChange.sink(receiveValue: { [weak self] in
+            DispatchQueue.main.async {
+                self?.titleTable.reloadData()
             }
-        }
+        })
     }
-    
+  
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return  myTitles?.getnumberOfRows() ?? 0
+        return viewModel.getnumberOfRows()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "titleCell", for: indexPath)
-        cell.textLabel?.text = myTitles?.getCellTitle(index: indexPath.row).getTitle()
+        cell.textLabel?.text = viewModel.getCellTitle(index: indexPath.row).getTitle()
         return cell
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    var myTitles: PostTitlesViewModel?
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        Task{
+//            do {
+//                myTitles = PostTitlesViewModel()
+//                try await myTitles?.getPostTitles()
+//                titleTable.reloadData()
+//            } catch let error as ServerError {
+//                print(error)
+//            }
+//        }
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return  myTitles?.getnumberOfRows() ?? 0
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "titleCell", for: indexPath)
+//        cell.textLabel?.text = myTitles?.getCellTitle(index: indexPath.row).getTitle()
+//        return cell
+//    }
 }
